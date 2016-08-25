73b1b3ffd520faf8622fd42cb4f53d243f96ba6a
Fernando Henrique Bandeira <fernando@agenciaget.com.br>
2016-08-25 09:58:25 -0300

    Iniciando service provider

--- src/LaravelPagseguroServiceProvider.php
+++ src/LaravelPagseguroServiceProvider.php
@@ -1,2 +1,13 @@
 <?php
 
+namespace PHPampa\laravelPagseguro;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelPagseguroServiceProvider extends ServiceProvider {
+	public function register() {
+		$this->app->bind('pagseguro', function($app) {
+			return new Pagseguro;
+		});
+	}
+}
