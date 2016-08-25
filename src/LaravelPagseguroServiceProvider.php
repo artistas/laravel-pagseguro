23a7578271f5c15a6bd75040739c953e26587c1c
Fernando Henrique Bandeira <fernando.bandeira@outlook.com>
2016-08-25 10:05:15 -0300

    Applied fixes from StyleCI (#1)

--- src/LaravelPagseguroServiceProvider.php
+++ src/LaravelPagseguroServiceProvider.php
@@ -4,10 +4,12 @@ namespace PHPampa\laravelPagseguro;
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelPagseguroServiceProvider extends ServiceProvider {
-	public function register() {
-		$this->app->bind('pagseguro', function($app) {
-			return new Pagseguro;
-		});
-	}
+class LaravelPagseguroServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind('pagseguro', function ($app) {
+            return new Pagseguro();
+        });
+    }
 }
