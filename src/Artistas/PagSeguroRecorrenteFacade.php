9b8d4d9416ee46899762a3a37ef582290850cd5d
Fernando Henrique Bandeira <fernando.bandeira@outlook.com>
2017-01-18 13:59:22 -0200

    Apply fixes from StyleCI (#37)

--- src/Artistas/PagSeguroRecorrenteFacade.php
+++ src/Artistas/PagSeguroRecorrenteFacade.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace Artistas\PagSeguro;
+
 use Illuminate\Support\Facades\Facade;
+
 class PagSeguroRecorrenteFacade extends Facade
 {
     protected static function getFacadeAccessor()
