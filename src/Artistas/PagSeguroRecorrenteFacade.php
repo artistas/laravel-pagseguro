9bc0c2ba825d00989da3f9156a2db577cb295452
Fernando Henrique Bandeira <fernando.bandeira@outlook.com>
2017-01-18 13:59:10 -0200

    Facade PagSeguro Recorrente

---
+++ src/Artistas/PagSeguroRecorrenteFacade.php
@@ -0,0 +1,10 @@
+<?php
+namespace Artistas\PagSeguro;
+use Illuminate\Support\Facades\Facade;
+class PagSeguroRecorrenteFacade extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'pagseguro_recorrente';
+    }
+}
