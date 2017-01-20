694bbfcd533449eba9e3874ca53469ffdd957505
Fernando Henrique Bandeira <fernando.bandeira@outlook.com>
2017-01-20 08:34:37 -0200

    Update PagSeguroClient.php

--- src/Artistas/PagSeguroClient.php
+++ src/Artistas/PagSeguroClient.php
@@ -59,14 +59,9 @@ class PagSeguroClient extends PagSeguroConfig
      */
     private function executeCurl($parameters, $url)
     {
-        $headers = [
-            'Accept: application/xml',
-            'Content-Type: application/x-www-form-urlencoded; charset=ISO-8859-1',
-        ];
-
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Content-Type: application/x-www-form-urlencoded; charset=ISO-8859-1']);
 
         if ($parameters !== null) {
             curl_setopt($curl, CURLOPT_POST, true);
